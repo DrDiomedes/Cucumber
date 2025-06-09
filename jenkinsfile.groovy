8dc6179e058b0383375fec41bbbcd45ddece374e
DrDiomedes <114020818+DrDiomedes@users.noreply.github.com>
2025-06-09 17:53:09 -0500

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -2,30 +2,42 @@ pipeline {
     agent any
 
     environment {
-        MAVEN_HOME = "${WORKSPACE}/apache-maven-3.8.6"
-        JAVA_HOME = "${WORKSPACE}/jdk-17"
-        PATH = "${JAVA_HOME}/bin:${MAVEN_HOME}/bin:${env.PATH}"
+        MAVEN_VERSION = '3.8.6'
+        JAVA_VERSION = '17'
+        MAVEN_DIR = "${WORKSPACE}/apache-maven-${MAVEN_VERSION}"
+        JAVA_DIR = "${WORKSPACE}/jdk-${JAVA_VERSION}"
+        PATH = "${JAVA_DIR}/bin:${MAVEN_DIR}/bin:${env.PATH}"
     }
 
     stages {
         stage('Instalar JDK y Maven') {
             steps {
                 sh '''
-                # Descargar e instalar Maven
-                curl -O https://downloads.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz
-                tar -xvzf apache-maven-3.8.6-bin.tar.gz
+                echo "[INFO] Descargando Maven..."
+                curl -sL https://archive.apache.org/dist/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz -o maven.tar.gz
+                tar -xzf maven.tar.gz
 
-                # Descargar e instalar JDK 17
-                curl -L -o jdk-17_linux-x64_bin.tar.gz https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz
-                tar -xvzf jdk-17_linux-x64_bin.tar.gz
+                echo "[INFO] Descargando JDK..."
+                curl -sL https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.10%2B7/OpenJDK17U-jdk_x64_linux_hotspot_17.0.10_7.tar.gz -o jdk.tar.gz
+                mkdir -p jdk-17 && tar -xzf jdk.tar.gz -C jdk-17 --strip-components=1
+
+                echo "[INFO] Instalación completada"
                 '''
             }
         }
 
         stage('Build & Test') {
             steps {
+                sh 'mvn -version'
+                sh 'java -version'
                 sh 'mvn clean test -Dbrowser=Chrome'
             }
+        }
+    }
+
+    post {
+        always {
+            junit '**/target/surefire-reports/*.xml'
         }
     }
 }
