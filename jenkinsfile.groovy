98930c3ac38ec11bc00440982d8b11417c68a9f0
DrDiomedes <114020818+DrDiomedes@users.noreply.github.com>
2025-06-09 18:48:38 -0500

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -36,9 +36,9 @@ pipeline {
                 sudo apt-get update -y
                 sudo apt-get install -y wget unzip curl gnupg
                 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-                sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
-                sudo apt-get update
-                sudo apt-get install -y google-chrome-stable
+                sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
+                apt-get update
+                apt-get install -y google-chrome-stable
 
                 echo "[INFO] Instalando ChromeDriver..."
                 CHROME_VERSION=$(google-chrome-stable --version | grep -oP '\\d+\\.\\d+\\.\\d+')
