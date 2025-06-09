b10c52e6d72f2983d4fdbdf864aef31908b9fb15
DrDiomedes <114020818+DrDiomedes@users.noreply.github.com>
2025-06-09 18:14:39 -0500

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -10,26 +10,26 @@ pipeline {
     }
 
     stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'feature/web-frontUrbano', url: 'https://bitbucket.org/mobilityado/fu-aragon.git'
+            }
+        }
+
         stage('Instalar JDK y Maven') {
             steps {
                 sh '''
-                echo "[INFO] Descargando Maven..."
                 curl -sL https://archive.apache.org/dist/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz -o maven.tar.gz
                 tar -xzf maven.tar.gz
 
-                echo "[INFO] Descargando JDK..."
                 curl -sL https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.10%2B7/OpenJDK17U-jdk_x64_linux_hotspot_17.0.10_7.tar.gz -o jdk.tar.gz
                 mkdir -p jdk-17 && tar -xzf jdk.tar.gz -C jdk-17 --strip-components=1
-
-                echo "[INFO] Instalación completada"
                 '''
             }
         }
 
         stage('Build & Test') {
             steps {
-                sh 'mvn -version'
-                sh 'java -version'
                 sh 'mvn clean test -Dbrowser=Chrome'
             }
         }
